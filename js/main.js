7978d8351618818b30d11a9ce9a2853f7c1e5263
Danielle Earnhardt <danielle.earnhardt3@gmail.com>
2016-11-30 15:40:43 -0500

    more responsive design changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var banner = document.getElementById('banner');
         var logo = document.getElementById('logo');
         var button1 = document.getElementById('button1');
         var button2 = document.getElementById('button2');
+        var hours = document.getElementById("info");
 
         button1.addEventListener("click", test);
         button2.addEventListener("click", test);
@@ -21,3 +22,11 @@ var banner = document.getElementById('banner');
                 logo.className = "show";
             }
         }
+
+if(screen.width >= "769") {
+    hours.className = "info"
+    console.log(screen.width);
+}
+else {
+    hours.className = "info-small"
+}
